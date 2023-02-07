d77c6338118bbad11a5e8fccfa2ea85fbf374265
lemonkang <53658981+lemonkang@users.noreply.github.com>
2023-02-07 14:06:46 +0800

    Feature/inno (#5)
    * dev分支第一次提交
    
    * 分支1第一次提交
    
    * dev1修改
    
    * 分支1修改
    
    ---------
    
    Co-authored-by: inno <Innok@karboncard.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 
-import { Button, Input } from "antd"
-import {useEffect, useRef, useState } from "react"
-import { useStateCallback } from "./hooks"
-const App=()=>{
-const {state:count,setStateCallback:setCount}=  useStateCallback<number>(1)
 
- 
-  const btnClick=()=>{
-    setCount(2,(val:any)=>{
-      console.log('====================================');
-      console.log('val',val);
-      console.log('====================================');
-    })
-const dev2="second commit"
-  }
+const App=()=>{
+
   return <div>
-    <div>master main dev 第二次提交</div>
-    <div>开发分支开发分支2修改</div>
-    <div>开发分支2</div>
+     <div>分支1第一次提交</div>
+     <div>分支1修改</div>
   </div>
 }
-export default App
+export default App 
