35d55c267d6cfd1e3750bb10b4ffd84fed4a1c64
inno <Innok@karboncard.com>
2023-02-07 12:05:22 +0800

    revert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 
-
+import { Button, Input } from "antd"
+import {useEffect, useRef, useState } from "react"
+import { useStateCallback } from "./hooks"
 const App=()=>{
+const {state:count,setStateCallback:setCount}=  useStateCallback<number>(1)
+
+ 
+  const btnClick=()=>{
+    setCount(2,(val:any)=>{
+      console.log('====================================');
+      console.log('val',val);
+      console.log('====================================');
+    })
 const dev2="second commit"
+  }
   return <div>
-     <div>分支1第一次提交</div>
+    <div>master main dev 第二次提交</div>
+    <div>开发分支开发分支2修改</div>
+    <div>开发分支2</div>
   </div>
 }
 export default App
