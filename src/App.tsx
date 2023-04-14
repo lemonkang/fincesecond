04886ed767d0fb5bd024c77b2302b47b931a8e3f
lemonkang <53658981+lemonkang@users.noreply.github.com>
2023-04-14 23:39:05 +0800

    Feature1 (#8)
    * feature第一次提交
    
    * feature1删除文件
    
    ---------
    
    Co-authored-by: inno <lemonkanghao@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import './App.css';
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(12)
   return ( 
     <div>
-   <button>maasdaser butth</button>
+   <button>masasdsadr</button>
   
 
   </div>
