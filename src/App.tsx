a2c1ec75128058d223041203603f969b5b619f02
inno <lemonkanghao@gmail.com>
2023-04-14 16:27:24 +0800

    masterthired 第二次修改

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 export const App = () => {
   return ( 
     <div>
-   <div>second 修改</div>
-   <div>second增加</div>
+   <div>mster </div>
+    <h2>masterxiugia </h2>
   
 
   </div>
