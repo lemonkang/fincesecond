4ce843fa4e888429bf0de44bf8efdf5f5d9eb705
lemonkang <53658981+lemonkang@users.noreply.github.com>
2023-04-14 16:14:06 +0800

    Feature/second (#7)
    * second 第一次提交
    
    * seocnd第二次修改
    
    ---------
    
    Co-authored-by: inno <lemonkanghao@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,10 @@ import './App.css';
 export const App = () => {
   return ( 
     <div>
-    <ul>
-      <li>a</li>
-      <li>b</li>
-      <li>c</li>
-      <li>d</li>
-      <li>e</li>
-      
-    </ul>
-    <div>first非法</div>
-    <div>first 继续开发</div>
-    <div>first开发结束</div>
+   <div>second 修改</div>
+   <div>second增加</div>
+  
+
   </div>
   )
 }
